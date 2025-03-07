7a8e15a1419533de2aa8c4f5b04693dd087d61eb
Jlili <lili.jie@funplus.biz>
2025-03-07 11:31:48 +0800

    feat: add base

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/image-viewer/',
   plugins: [
     vue(),
     AutoImport({
